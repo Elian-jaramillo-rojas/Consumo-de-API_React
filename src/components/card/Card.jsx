2b53d68c0aaa777dc5bcd83d1495dc78f2a4ff13
Elian-jaramillo-rojas <ejaramillo62@misena.edu.co>
2021-12-10 21:27:18 -0500

    se agrego el css

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,11 +6,15 @@ export const Card = (data) => {
     const { name, image,species } = data.data;
   
     return (
+      <div className='contenedor1'>
+
+      
         <div className="card">
-        <img src={image}  />
+        <img  src={image}  />
         <h2>{name}</h2>
         <hr />
         <p>{species}</p>
       </div>
+    </div>
     )
   }
